0bcda6f86721978ce640a1536e2aba5414b6f2fc
szefoski <zielas.daniel@gmail.com>
2025-11-13 20:44:57 +0100

    Add demangle utility functions for type names

---
+++ demangle.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <cxxabi.h>
+#include <memory>
+#include <typeinfo>
+#include <iostream>
+#include <string_view>
+
+template<typename T>
+std::string demangle() {
+    int status;
+    std::unique_ptr<char, void(*)(void*)> res{
+        abi::__cxa_demangle(typeid(T).name(), nullptr, nullptr, &status),
+        std::free
+    };
+    return (status == 0) ? res.get() : typeid(T).name();
+}
+
+template<typename T>
+std::string demangle(const T &t) {
+    int status;
+    std::unique_ptr<char, void(*)(void*)> res{
+        abi::__cxa_demangle(typeid(T).name(), nullptr, nullptr, &status),
+        std::free
+    };
+    return (status == 0) ? res.get() : typeid(T).name();
+}
+
+template<typename T>
+void demangle_print(const T &t, const std::string_view name = "Type") {
+    std::cout << name << ": " << demangle(t) << std::endl;
+}
