ee6eeb123e5f91111fccb96948fedd8dc499f396
szefoski <zielas.daniel@gmail.com>
2025-11-13 20:46:47 +0100

    Remove demangle function overload
    Removed the overloaded demangle function without parameters.

--- demangle.hpp
+++ demangle.hpp
@@ -1,20 +1,15 @@
 #pragma once
 
+namespace szefoski
+{
+nemespace tools
+{
+
 #include <cxxabi.h>
 #include <memory>
 #include <typeinfo>
 #include <iostream>
 #include <string_view>
-
-template<typename T>
-std::string demangle() {
-    int status;
-    std::unique_ptr<char, void(*)(void*)> res{
-        abi::__cxa_demangle(typeid(T).name(), nullptr, nullptr, &status),
-        std::free
-    };
-    return (status == 0) ? res.get() : typeid(T).name();
-}
 
 template<typename T>
 std::string demangle(const T &t) {
@@ -29,4 +24,7 @@ std::string demangle(const T &t) {
 template<typename T>
 void demangle_print(const T &t, const std::string_view name = "Type") {
     std::cout << name << ": " << demangle(t) << std::endl;
+}
+
+}
 }
